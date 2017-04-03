c38cb921459e4c564fdd740c356a99626c3986dc
TaoBeier <zhangjintao9020@gmail.com>
2017-04-03 23:38:44 +0800

    stash

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Main from '@/views/Main';
+import Index from '@/views/Index';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/main',
+      name: 'Main',
+      component: Main,
+      children: [
+        {
+          path: '/',
+          name: 'Index',
+          component: Index,
+        },
+      ],
+    },
+  ],
+});
